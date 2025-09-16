a551f68eaaeec331d37f72604affc1e87a487cef
Georgi Komov <gogo5f7@gmail.com>
2025-09-16 22:08:06 +0300

    implemented more difficulty

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,38 +1,53 @@
 import random
 
-number_to_guess = random.randint(1, 100) # Chooses a random number from 1 to 100
-difficulty = input("Choose a diffuculty (e), (m), (h): ") # asks the player for a  difficulty 
+while True: #Main loop which checks if the player wants to play
 
-guesses = 0
+     # Chooses a random number from 1 to 100
+    number_to_guess = random.randint(1, range_max)
+
+    difficulty = input("Choose a diffuculty (e), (m), (h): ") # asks the player for a  difficulty 
 
 # Difficulty logic
-if difficulty == "e":
-    max_guesses = 15
-elif difficulty == "m":
-    max_guesses = 10
-elif difficulty == "h":
-    max_guesses = 5
-else:
-    max_guesses = 10
+    if difficulty == "e":
+        range_max = 100
+        max_guesses = 15
+    elif difficulty == "m": 
+        range_max = 200
+        max_guesses = 10
+    elif difficulty == "h":
+        range_max = 300
+        max_guesses = 5
+    else:
+        range_max = 200
+        max_guesses = 10
+    
+    guesses = 0
 
-while guesses < max_guesses:
-    number = int(input("Guess a number: "))
-    guesses += 1
+    # Loop for the game logic
+    while guesses < max_guesses:
+        number = int(input("Guess a number: "))
+        guesses += 1
 
-    if number > number_to_guess:
-        print("Too high")
-    elif number < number_to_guess:
-         print("Too low")
-    elif number == number_to_guess:
-        print("-------------")
-        print("You Win!")
-        print(f"Guesses: {guesses}")
+        if number > number_to_guess:
+            print("Too high")
+        elif number < number_to_guess:
+            print("Too low")
+        else:
+            print("-------------")
+            print("You Win!")
+            print(f"Guesses: {guesses}")
+            print(f"The number was: {number_to_guess}")
+            break
+    else:   
+        print("-----------------")
+        print("Out of guesses")
         print(f"The number was: {number_to_guess}")
+        print("-----------------")
+        
+    
+    play_again = input("Do you want to keep playin? (y), (n): ")
+    if play_again != "y":
+        print("Thanks for playing!")
         break
-else:
-    print("-----------------")
-    print("Out of guesses")
-    print(f"The number was: {number_to_guess}")
-    print("-----------------")
 
 

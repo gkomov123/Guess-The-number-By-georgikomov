38a20306d199737c84b2cb1bacbe9db09a53dec3
Georgi Komov <gogo5f7@gmail.com>
2025-09-16 21:55:04 +0300

    added difficulties

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,10 +1,21 @@
 import random
 
-number_to_guess = random.randint(1, 100)
+number_to_guess = random.randint(1, 100) # Chooses a random number from 1 to 100
+difficulty = input("Choose a diffuculty (e), (m), (h): ") # asks the player for a  difficulty 
 
 guesses = 0
 
-while True:
+# Difficulty logic
+if difficulty == "e":
+    max_guesses = 15
+elif difficulty == "m":
+    max_guesses = 10
+elif difficulty == "h":
+    max_guesses = 5
+else:
+    max_guesses = 10
+
+while guesses < max_guesses:
     number = int(input("Guess a number: "))
     guesses += 1
 
@@ -13,9 +24,15 @@ while True:
     elif number < number_to_guess:
          print("Too low")
     elif number == number_to_guess:
+        print("-------------")
         print("You Win!")
         print(f"Guesses: {guesses}")
+        print(f"The number was: {number_to_guess}")
         break
-
+else:
+    print("-----------------")
+    print("Out of guesses")
+    print(f"The number was: {number_to_guess}")
+    print("-----------------")
 
 

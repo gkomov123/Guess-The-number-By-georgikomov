fa49f5033d7207d7b2037dd3dc1b6c31de137877
Georgi Komov <gogo5f7@gmail.com>
2025-09-16 22:25:38 +0300

    added a time based difficulty

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,11 +1,9 @@
 import random
+import time
 
 while True: #Main loop which checks if the player wants to play
-
-     # Chooses a random number from 1 to 100
-    number_to_guess = random.randint(1, range_max)
-
-    difficulty = input("Choose a diffuculty (e), (m), (h): ") # asks the player for a  difficulty 
+    
+    difficulty = input("Choose a diffuculty (e), (m), (h), (c), (i), (t): ").lower() # asks the player for a  difficulty 
 
 # Difficulty logic
     if difficulty == "e":
@@ -17,14 +15,35 @@ while True: #Main loop which checks if the player wants to play
     elif difficulty == "h":
         range_max = 300
         max_guesses = 5
+    elif difficulty == "c":
+        range_max = int(input("Enter the maximum number: "))
+        max_guesses = int(input("Enter how many guesses you want: "))
+    elif difficulty == "i":
+        range_max = 1000
+        max_guesses = 1
+    elif difficulty == "t":
+        range_max = 100
+        max_guesses = float("inf")
+        time_limit = 30 #SECONDS
+        start_time = time.time()
+        
     else:
         range_max = 200
         max_guesses = 10
+
+    # Chooses a random number from 1 to 100
+    number_to_guess = random.randint(1, range_max)    
     
     guesses = 0
 
+
     # Loop for the game logic
     while guesses < max_guesses:
+
+        if difficulty == "t" and (time.time() - start_time) > time_limit:
+            print("⏰ Time's up! You lose.")
+            break
+
         number = int(input("Guess a number: "))
         guesses += 1
 
@@ -37,6 +56,7 @@ while True: #Main loop which checks if the player wants to play
             print("You Win!")
             print(f"Guesses: {guesses}")
             print(f"The number was: {number_to_guess}")
+            print(f"It took you, {int(time.time() - start_time)}s, to guess the number")
             break
     else:   
         print("-----------------")
